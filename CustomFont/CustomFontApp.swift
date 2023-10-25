51be6556065b961889761600f87e0ad43d120d74
Pac12Run8000 <gangstarr78992@>
2023-10-25 11:16:46 -0700

    Initial Commit

---
+++ CustomFont/CustomFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontApp.swift
+//  CustomFont
+//
+//  Created by Michelle Grover on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
